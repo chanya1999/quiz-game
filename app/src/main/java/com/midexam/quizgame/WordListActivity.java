6f1fc415fe335c138e5b76ae4f52c75945f96230
chanya1999 <nakubchanya@gmail.com>
2020-09-20 14:40:55 +0700

    Initial commit

--- app/src/main/java/com/midexam/quizgame/WordListActivity.java
+++ app/src/main/java/com/midexam/quizgame/WordListActivity.java
@@ -1,8 +1,16 @@
 package com.midexam.quizgame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.midexam.quizgame.model.WordItem;
 
 public class WordListActivity extends AppCompatActivity {
 
@@ -10,5 +18,42 @@ public class WordListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_list);
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+        WordItem item;
+        public MyAdapter() {
+            item = new WordItem(R.drawable.cat,"CAT");
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_word, parent, false);
+            MyViewHolder vh = new MyViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder{
+            ImageView imageView;
+            TextView wordTextView;
+
+            public MyViewHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView = itemView.findViewById(R.id.image_view);
+                wordTextView = itemView.findViewById(R.id.word_text_view);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/midexam/quizgame/model/WordItem.java
@@ -0,0 +1,11 @@
+package com.midexam.quizgame.model;
+
+public class WordItem {
+    int imageResId;
+    String word;
+
+    public WordItem(int imageResId, String word) {
+        this.imageResId = imageResId;
+        this.word = word;
+    }
+}
