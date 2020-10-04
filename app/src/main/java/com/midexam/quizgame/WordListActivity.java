ab4b5ac8ce737f6fde65c6c2ca770b4c6a267df3
chanya1999 <nakubchanya@gmail.com>
2020-10-04 11:25:18 +0700

    Initial commit

--- app/src/main/java/com/midexam/quizgame/WordListActivity.java
+++ app/src/main/java/com/midexam/quizgame/WordListActivity.java
@@ -4,12 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.midexam.quizgame.model.WordItem;
 
@@ -21,18 +24,18 @@ public class WordListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_word_list);
 
         //Create Adapter object
-        MyAdapter adapter = new MyAdapter();
+        MyAdapter adapter = new MyAdapter(WordListActivity.this);
         //Create layout manager
         LinearLayoutManager lm = new LinearLayoutManager(WordListActivity.this);
         RecyclerView rv = findViewById(R.id.word_list_recycler_view);
         rv.setLayoutManager(lm);
         rv.setAdapter(adapter);
     }
-
-
 }
 
 class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    final Context mContext;
 
     WordItem[] items = {
             new WordItem(R.drawable.cat,"CAT"),
@@ -46,8 +49,8 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             new WordItem(R.drawable.penguin,"PENGUIN"),
             new WordItem(R.drawable.pig,"PIG")
     };
-    public MyAdapter() {
-
+    public MyAdapter(Context context) {
+        this.mContext = context;
     }
 
     @NonNull
@@ -77,6 +80,14 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             super(itemView);
             imageView = itemView.findViewById(R.id.image_view);
             wordTextView = itemView.findViewById(R.id.word_text_view);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast t = Toast.makeText(mContext,"Hello",Toast.LENGTH_LONG);
+                    t.show();
+                }
+            });
         }
     }
 }
