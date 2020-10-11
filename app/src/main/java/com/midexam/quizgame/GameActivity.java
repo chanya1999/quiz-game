d6f78b65f05ce383b8b5351f213168cbd0442602
chanya1999 <nakubchanya@gmail.com>
2020-10-11 08:30:05 +0700

    Initial commit

--- app/src/main/java/com/midexam/quizgame/GameActivity.java
+++ app/src/main/java/com/midexam/quizgame/GameActivity.java
@@ -3,59 +3,88 @@ package com.midexam.quizgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.midexam.quizgame.model.WordItem;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView mQuestionImageView;
-    private Button[] mChoiceButtons = new Button[4];
+    private Button[] mButtons = new Button[4];
+
+    private String mAnswerWord;
+    private Random mRandom;
+    private List<WordItem> mItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        List<WordItem> itemList = new ArrayList<>(Arrays.asList(WordListActivity.items));
-
         mQuestionImageView = findViewById(R.id.question_image_view);
-        mChoiceButtons[0] = findViewById(R.id.choice_1_button);
-        mChoiceButtons[1] = findViewById(R.id.choice_2_button);
-        mChoiceButtons[2] = findViewById(R.id.choice_3_button);
-        mChoiceButtons[3] = findViewById(R.id.choice_4_button);
+        mButtons[0] = findViewById(R.id.choice_1_button);
+        mButtons[1] = findViewById(R.id.choice_2_button);
+        mButtons[2] = findViewById(R.id.choice_3_button);
+        mButtons[3] = findViewById(R.id.choice_4_button);
 
-        Random r = new Random();
+        mButtons[0].setOnClickListener(this);
+        mButtons[1].setOnClickListener(this);
+        mButtons[2].setOnClickListener(this);
+        mButtons[3].setOnClickListener(this);
+
+
+        mRandom = new Random();
+        newQuiz();
+    }
+
+    private void newQuiz() {
+        mItemList = new ArrayList<>(Arrays.asList(WordListActivity.items));
         // random word for game
-        int answerIndex = r.nextInt(itemList.size());
+        int answerIndex = mRandom.nextInt(mItemList.size());
 
-        WordItem item = itemList.get(answerIndex);
+        WordItem item = mItemList.get(answerIndex);
         // set image for test
         mQuestionImageView.setImageResource(item.imageResId);
 
+        mAnswerWord = item.word;
+
         // random choice button answer
-        int randomButton = r.nextInt(4);
-        mChoiceButtons[randomButton].setText(itemList.get(answerIndex).word);
+        int randomButton = mRandom.nextInt(4);
+        mButtons[randomButton].setText(item.word);
         // pull answer item out form list
-        itemList.remove(item);
+        mItemList.remove(item);
         // shuffle data
-        Collections.shuffle(itemList);
+        Collections.shuffle(mItemList);
 
         for (int i = 0; i < 4 ; i++){
             if(i == randomButton){
                 continue;
             }
-            mChoiceButtons[i].setText(itemList.get(i).word);
+            mButtons[i].setText(mItemList.get(i).word);
         }
+    }
 
+
+    @Override
+    public void onClick(View view) {
+        Button b = findViewById(view.getId());
+        String buttonText = b.getText().toString();
+
+        if(mAnswerWord.equals(buttonText)){
+            Toast.makeText(GameActivity.this,"✔ CORRECT",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(GameActivity.this,"✖ INCORRECT!",Toast.LENGTH_SHORT).show();
+        }
+        newQuiz();
 
     }
 }
